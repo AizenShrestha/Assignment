ce49da8986acb746aeed8acbeb72b08f69caab77
unknown <shresthaaizen25@gmail.com>
2024-05-02 07:30:20 +0545

    pagelinked

--- React/myapp/src/app/register/page.js
+++ React/myapp/src/app/register/page.js
@@ -1,22 +1,25 @@
+'use client'
 import CustomNavbar from "@/components/navbar/page";
 import {Button,Input} from "@nextui-org/react";
 import Link from "next/link";
 Link
 
-const Login = () => {
+const Register = () => {
   return (
     <div>
       <CustomNavbar/>
       <div className="flex justify-center ">
       <div className='border border-black m-7 w-72 p-4 '>
+          <Input type="email" label="Email" /> <br/>
           <Input label="Username" /> <br/>
+          <Input  label="Address" /> <br/>
           <Input type="password" label="Password" /> <br/>
           <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">
-        Login
+        Sign Up
       </Button>
       <br/>
       <br/>
-      <div class="decoration-solid text-sm">Don't have an Account yet? <Link href="/register">Sign Up</Link></div>
+      <div class="decoration-solid text-sm">Already have an Account? <Link href="/login">Sign In here</Link></div>
       </div>
       </div>
     </div>
@@ -24,4 +27,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Register
---
+++ React/myapp/src/components/navbar/logo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const logo =()=> {
+    return (
+        <div className='bg-teal-600 p-2 rounded'>GIZMO</div>
+
+    )
+    
+}
+export default logo
